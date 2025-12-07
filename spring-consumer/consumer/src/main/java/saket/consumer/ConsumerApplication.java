a4ca1ee4c234635209155ae9934a0dda9c695895
Saket Suman <ssaket121@gmail.com>
2025-12-07 03:33:13 -0500

    added some comments

--- spring-consumer/consumer/src/main/java/saket/consumer/ConsumerApplication.java
+++ spring-consumer/consumer/src/main/java/saket/consumer/ConsumerApplication.java
@@ -5,7 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ConsumerApplication {
-
+	/**
+	 * The springboot application entry point.
+	 * @param args cmdline args.
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(ConsumerApplication.class, args);
 	}
